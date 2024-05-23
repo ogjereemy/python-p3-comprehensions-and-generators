85da4624d0601a61b292ec35923aab7d620d0629
ogola <ogolajeremy601@gmail.com>
2024-05-23 07:49:47 +0300

    adds comprehensions and gen

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    return [i for i in num_list if i % 2 == 0]
+    # pass
 
 def make_exclamation(sentence_list):
-    pass
+    return [i + "!" for i in sentence_list]
+    # pass
+
